17c11487a3765cd2b325ec5fe558d7295669a9fd
Christian Reis <christianrfg@gmail.com>
2018-10-20 18:56:14 -0300

    Fix Attributes
    Create and deletion of some attributes. Changes some variable names too.

--- python_codes/particle.py
+++ python_codes/particle.py
@@ -3,34 +3,31 @@ class Particle:
     Description
     ---------
     Class that represents one particle in the PSO.
-    
+
     Attributes
     ---------
     position: numpy array
         Uniformly distributed random vector.
     velocity: float
         Particle's velocity.
-    p_i: float
+    best_position: float
         Particle's best known position.
+    neighbors_particles: list of Particle
+        Neighbors of that Particle.
     """
-    def __init__(self, position, velocity, p_i):
+
+    def __init__(self, position, velocity, best_position):
         self.position = position
         self.velocity = velocity
-        self.p_i = p_i
+        self.best_position = best_position
+        self.neighbors_particles = None
 
     def __repr__(self):
-        return "x: {0}\nVelocity: {1}\np_i: {2}\n".format(
-            self.x, self.velocity, self.p_i)
-    
+        position_neighbors = [x.position for x in self.neighbors_particles]
+        return "Particle position: {0}\nVelocity: {1}\nBest position: {2}\nPositions of neighboring particles:\n{3}".format(
+            self.position, self.velocity, self.best_position, position_neighbors)
+
     def __str__(self):
-        return "x: {0}\nVelocity: {1}\np_i: {2}\n".format(
-            self.x, self.velocity, self.p_i)
-
-    def set_x(self, x):
-        self.x = x
-
-    def set_velocity(self, velocity):
-        self.velocity = velocity
-        
-    def set_p_i(self, p_i):
-        self.p_i = p_i
+        position_neighbors = [x.position for x in self.neighbors_particles]
+        return "Particle position: {0}\nVelocity: {1}\nBest position: {2}\nPositions of neighboring particles:\n{3}".format(
+            self.position, self.velocity, self.best_position, position_neighbors)
